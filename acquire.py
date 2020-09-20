0c54f5963128149201420b15996661ed946902c4
Matthew-Mays <matthew.c.mays96@gmail.com>
2020-09-19 20:02:48 -0500

    fixed a couple problems in acquire

--- acquire.py
+++ acquire.py
@@ -9,7 +9,7 @@ import pandas as pd
 import os
 
 
-# In[3]:
+# In[13]:
 
 
 # Make a function named get_titanic_data that returns the titanic data from the codeup data science 
@@ -18,33 +18,33 @@ import os
 def get_connection(db, user=env.username, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
     
-def get_titanic_data(cached=False):
+def get_titanic_data():
     filename = 'titanic.csv'
-    if cached or os.path.isfile(filename) == False:
+    if os.path.isfile(filename) == False:
         df = pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
         df.to_csv(filename)
-        return df
     else:
-        return pd.read_csv(filename)
-        
+        df = pd.read_csv('titanic.csv', index_col=0)
+    return df
 
 
-# In[4]:
+# In[14]:
 
 
 # Make a function named get_iris_data that returns the data from the iris_db on the codeup data science database 
 # as a pandas data frame. The returned data frame should include the actual name of the species in addition 
 # to the species_ids. Obtain your data from the Codeup Data Science Database.
 
-def get_iris_data(cached=False):
+def get_iris_data():
     filename = 'iris.csv'
-    if cached or os.path.isfile(filename) == False:
-        return pd.read_csv(filename)
+    sql_query = '''SELECT species_id, species_name, sepal_length, sepal_width, petal_length, petal_width FROM
+                measurements JOIN species USING(species_id)'''
+    if os.path.isfile(filename) == False:
+        df = pd.read_sql(sql_query, get_connection('iris_db'))
     else:
-        df = pd.read_sql('SELECT * FROM measurements as m JOIN species AS s ON m.species_id = s.species_id'
-                         ,get_connection('iris_db'))
+        df = pd.read_csv(filename)
         df.to_csv(filename)
-        return df
+    return df
 
 
 # In[5]:
@@ -54,10 +54,4 @@ def get_iris_data(cached=False):
 # To do this, edit the beginning of the function to check for a local filename like titanic.csv or iris.csv. 
 # If they exist, use the .csv file. If the file doesn't exist, then produce the SQL and pandas necessary 
 # to create a dataframe, then write the dataframe to a .csv file with the appropriate name.
-
-
-# In[ ]:
-
-
-
 
